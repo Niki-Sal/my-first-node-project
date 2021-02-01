fdbf41e01fee14a1af743d4fde3b98d68c1aa4aa
niki-sal <ne.salehi@gmail.com>
2021-02-01 10:51:08 -0800

    add other modules

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const myModule = require("./myModule")
+const otherModule = require("./otherModule")
+
+
 console.log ("Hello World");
 
 function hello(name){
@@ -6,3 +10,7 @@ function hello(name){
 
 hello('Mocha');
 hello("JC")
+
+console.log(myModule.add(2,2))
+console.log(myModule.subtract(5,4))
+otherModule.greeting("Sam")
